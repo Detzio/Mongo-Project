cac1b4822491edb1281ed89d8bbdccc411d8b239
AlexandreCrestien <alexandre.crestien@gmail.com>
2024-09-26 09:48:35 +0200

    [+] add  comment model and post model

--- model/comment.model.js
+++ model/comment.model.js
@@ -3,3 +3,14 @@
  * 
  * Un post doit avoir au minimum : un message, une date, un userId (auteur du post) et un postId
  */
+
+const mongoose = require('mongoose');
+
+const Comment = mongoose.Schema({
+    message: { type: String, required: true },
+    date: { type: Date, required: true },
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    postId: {type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true},
+});
+
+module.exports =  mongoose.model('Comment',Comment);
--- model/post.model.js
+++ model/post.model.js
@@ -3,3 +3,13 @@
  * 
  * Un post doit avoir au minimum : un message, une date, un userId (auteur du post)
  */
+
+const mongoose = require('mongoose');
+
+const Post = mongoose.Schema({
+    message: { type: String, required: true },
+    date: { type: Date, required: true },
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+});
+
+module.exports =  mongoose.model('Post',Post);
