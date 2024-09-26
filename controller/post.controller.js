046aac1a223fa66117ee3eb375c44154d11a1d16
Dusséaux Thomas <t.dusseaux@gmail.com>
2024-09-26 12:12:26 +0200

    Finish with saves

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -28,18 +28,23 @@ exports.getAll = async (req, res) => {
  * Methode pour récupérer un post par son id, et les commentaires associés à ce post
  */
 exports.getById = async (req, res) => {
-    try{
-        let result = await Post.findById(req.params.id)
-        await Comment.find({postId : req.params.id}); 
-        if(result == null) {
-            res.status(404).send("post not found")
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).send("Post not found");
         }
-        res.status(200).json(postWithComment);
-    }catch(e){
-        res.status(500).json(e.message);
+
+        const comments = await Comment.find({ postId: req.params.id });
+        const postWithComments = {
+            post,
+            comments
+        };
+
+        res.status(200).json(postWithComments);
+    } catch (e) {
+        res.status(500).json({ message: e.message });
     }
 }
-
 /**
  * Methode pour créer un nouveau post (attention à bien enregistrer la date de création)
  * @body
